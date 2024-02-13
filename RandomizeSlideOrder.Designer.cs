e105d54c25dd991d2e17d585df96f8953beba7a6
Windspar <jake@sccomputers.com>
2024-02-12 23:11:35 -0800

    Initial commit

---
+++ RandomizeSlideOrder.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace RandomizeSlideOrder
+{
+    partial class RandomizeSlideOrder : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public RandomizeSlideOrder()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RandomizeSlideOrder));
+            this.tabSlideOrder = this.Factory.CreateRibbonTab();
+            this.slideOrder = this.Factory.CreateRibbonGroup();
+            this.shuffleAndStart = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.shuffleAndLoop = this.Factory.CreateRibbonButton();
+            this.separator2 = this.Factory.CreateRibbonSeparator();
+            this.resetButton = this.Factory.CreateRibbonButton();
+            this.tabSlideOrder.SuspendLayout();
+            this.slideOrder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabSlideOrder
+            // 
+            this.tabSlideOrder.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabSlideOrder.ControlId.OfficeId = "TabSlideShow";
+            this.tabSlideOrder.Groups.Add(this.slideOrder);
+            this.tabSlideOrder.Label = "TabSlideShow";
+            this.tabSlideOrder.Name = "tabSlideOrder";
+            // 
+            // slideOrder
+            // 
+            this.slideOrder.Items.Add(this.shuffleAndStart);
+            this.slideOrder.Items.Add(this.separator1);
+            this.slideOrder.Items.Add(this.shuffleAndLoop);
+            this.slideOrder.Items.Add(this.separator2);
+            this.slideOrder.Items.Add(this.resetButton);
+            this.slideOrder.Label = "Slide Order";
+            this.slideOrder.Name = "slideOrder";
+            this.slideOrder.Position = this.Factory.RibbonPosition.AfterOfficeId("GroupSlideShowStart");
+            // 
+            // shuffleAndStart
+            // 
+            this.shuffleAndStart.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.shuffleAndStart.Image = global::RandomizeSlideOrder.Properties.Resources.shuffle;
+            this.shuffleAndStart.Label = "Shuffle Start";
+            this.shuffleAndStart.Name = "shuffleAndStart";
+            this.shuffleAndStart.ScreenTip = "Shuffles the order of the presentation and starts it";
+            this.shuffleAndStart.ShowImage = true;
+            this.shuffleAndStart.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ShuffleOrderAndPlay);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // shuffleAndLoop
+            // 
+            this.shuffleAndLoop.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.shuffleAndLoop.Image = global::RandomizeSlideOrder.Properties.Resources.loop;
+            this.shuffleAndLoop.Label = "Shuffle Loop";
+            this.shuffleAndLoop.Name = "shuffleAndLoop";
+            this.shuffleAndLoop.ScreenTip = "Shuffles te presentation order and continuously loops until exiting";
+            this.shuffleAndLoop.ShowImage = true;
+            this.shuffleAndLoop.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.LoopOrder);
+            // 
+            // separator2
+            // 
+            this.separator2.Name = "separator2";
+            // 
+            // resetButton
+            // 
+            this.resetButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.resetButton.Image = ((System.Drawing.Image)(resources.GetObject("resetButton.Image")));
+            this.resetButton.Label = "Reset Order";
+            this.resetButton.Name = "resetButton";
+            this.resetButton.ShowImage = true;
+            this.resetButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ResetOrder);
+            // 
+            // RandomizeSlideOrder
+            // 
+            this.Name = "RandomizeSlideOrder";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tabSlideOrder);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.LoadRibbon);
+            this.tabSlideOrder.ResumeLayout(false);
+            this.tabSlideOrder.PerformLayout();
+            this.slideOrder.ResumeLayout(false);
+            this.slideOrder.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabSlideOrder;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup slideOrder;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton shuffleAndStart;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton shuffleAndLoop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton resetButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator2;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal RandomizeSlideOrder Ribbon1
+        {
+            get { return this.GetRibbon<RandomizeSlideOrder>(); }
+        }
+    }
+}
---
+++ RandomizeSlideOrder.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using Application = Microsoft.Office.Interop.PowerPoint.Application;
+
+namespace RandomizeSlideOrder
+{
+    public partial class RandomizeSlideOrder
+    {
+        Application powerpoint;
+        private void LoadRibbon(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+        private void ShuffleOrderAndPlay(object sender, RibbonControlEventArgs e)
+        {
+            powerpoint = Globals.ThisAddIn.Application;
+            powerpoint.StartNewUndoEntry();
+            SlideShowSettings presentation = ShuffleOrder();
+            if (presentation == null) return;
+            presentation.Run();
+        }
+        private void LoopOrder(object sender, RibbonControlEventArgs e)
+        {
+            powerpoint = Globals.ThisAddIn.Application;
+            powerpoint.StartNewUndoEntry();
+            SlideShowSettings presentation = LoopOrder();
+            if (presentation == null) return;
+            presentation.Run();
+            
+        }
+        private void ResetOrder(object sender, RibbonControlEventArgs e)
+        {
+            if(powerpoint == null) return;
+            try
+            {
+                powerpoint.CommandBars.ExecuteMso("Undo");
+            }
+            catch(Exception) {}
+        }
+        private SlideShowSettings ShuffleOrder()
+        {
+            powerpoint = Globals.ThisAddIn.Application;
+            Slides slides = powerpoint.ActivePresentation.Slides;
+            if (slides.Count > 0)
+            {
+                Random random = new Random();
+                for (int i = 1; i <= slides.Count; i++)
+                {
+                    try
+                    {
+                        int randNum = random.Next(i, slides.Count);
+                        slides[randNum].Cut();
+                        slides.Paste(1);
+                    }
+                    catch (Exception) {}
+                }
+                return powerpoint.ActivePresentation.SlideShowSettings;
+            }
+            return null;
+        }
+        private SlideShowSettings LoopOrder()
+        {
+            powerpoint = Globals.ThisAddIn.Application;
+            powerpoint.StartNewUndoEntry();
+            Slides slides = powerpoint.ActivePresentation.Slides;
+            if (slides.Count > 0)
+            {
+                Random random = new Random();
+                for (int i = 1; i <= slides.Count; i++)
+                {
+                    int randNum = random.Next(i, slides.Count);
+                    slides[randNum].Cut();
+                    slides.Paste(1);
+                }
+                powerpoint.ActivePresentation.SlideShowSettings.LoopUntilStopped = MsoTriState.msoTrue;
+                return powerpoint.ActivePresentation.SlideShowSettings;
+            }
+            return null;
+        }
+    }
+}
